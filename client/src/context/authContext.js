172e6785d66c110fd43e6452b54d2d37a622b67e
Rumi <moonding901212@gmail.com>
2024-02-26 21:48:57 +0900

    인증정보 context

--- client/src/context/authContext.js
+++ client/src/context/authContext.js
@@ -1,29 +1,23 @@
 import { createContext, useEffect, useState } from "react";
-
+import axios from "axios";
 export const AuthContext = createContext();
-
+/**
+ * 사용자 인증 정보를 저장하고 공유하기 위한 Context 객체
+ */
 export const AuthContextProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(
-    JSON.parse(localStorage.getItem("user")) || null
-  );
+  const [currentUser, setCurrentUser] = useState(JSON.parse(localStorage.getItem("user")) || null);
 
-  const login = () => {
-    //TO DO
-    setCurrentUser({
-      id: 1,
-      name: "John Doe",
-      profilePic:
-        "https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  const login = async (inputs) => {
+    const res = await axios.post("http://localhost:8800/api/auth/login", inputs, {
+      withCredentials: true, //쿠키로 작업할때는 이것 넣어줘야함
     });
+
+    setCurrentUser(res.data);
   };
 
   useEffect(() => {
     localStorage.setItem("user", JSON.stringify(currentUser));
   }, [currentUser]);
 
-  return (
-    <AuthContext.Provider value={{ currentUser, login }}>
-      {children}
-    </AuthContext.Provider>
-  );
+  return <AuthContext.Provider value={{ currentUser, login }}>{children}</AuthContext.Provider>;
 };
