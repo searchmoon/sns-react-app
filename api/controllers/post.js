6449699ed8455e8cef1a23cd7a4d7ea97f127a5d
Rumi <moonding901212@gmail.com>
2024-02-27 15:44:22 +0900

    post 기능 추가

---
+++ api/controllers/post.js
@@ -0,0 +1,43 @@
+import { db } from "../connect.js";
+import jwt from "jsonwebtoken";
+import moment from "moment";
+
+export const getPosts = (req, res) => {
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Not logged in!");
+
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
+
+    const q = `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) 
+    LEFT JOIN relationships AS r ON (p.userId = r.followedUserId) WHERE r.followerUserId = ? OR p.userId =?
+    ORDER BY p.createdAt DESC`;
+
+    db.query(q, [userInfo.id, userInfo.id], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json(data);
+    });
+  });
+};
+
+export const addPost = (req, res) => {
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Not logged in!");
+
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    // auth.js에서 jwt.sign({ id: data[0].id }, "secretkey")의 첫번째 인자인 객체를 받아서 가져다 쓰게된다. userInfo.id 를
+    if (err) return res.status(403).json("Token is not valid!");
+    const q = "INSERT INTO posts(`desc`, `img`, `createdAt`, `userId`) VALUES (?)";
+    const values = [
+      req.body.desc,
+      req.body.img,
+      moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+      userInfo.id,
+    ];
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json("Post has been created.");
+    });
+  });
+};
+export const deletePost = (req, res) => {};
