dfde42b4f40912006640e0543440a715f936e4f2
Rumi <moonding901212@gmail.com>
2024-02-28 16:41:02 +0900

    user 상세 기능 추가

---
+++ api/controllers/user.js
@@ -0,0 +1,16 @@
+import { db } from "../connect.js";
+import jwt from "jsonwebtoken";
+
+export const getUser = (req, res) => {
+  const userId = req.params.userId;
+  const q = "SELECT * FROM users WHERE id=?";
+
+  db.query(q, [userId], (err, data) => {
+    if (err) return res.status(500).json(err);
+    const { password, ...info } = data[0];
+    console.log(data);
+    return res.json(info);
+  });
+};
+
+export const updateUser = (req, res) => {};
